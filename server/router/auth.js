56da6755c9dbf97455f4080077849eda2ce27afc
amit-g0swami <goswamiempire@gmail.com>
2022-05-11 09:22:36 +0530

    push data using async/await

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,24 +8,46 @@ router.get("/", (req, res) => {
     res.send("hello")
 })
 
-router.post("/register", (req, res) => {
+// using Promise 
+// router.post("/register", (req, res) => {
+//     const { name, email, phone, work, password } = req.body;
+
+//     if (!name || !email || !phone || !work || !password) { return res.status(422).json({ "error": "plz fill the form" }) }
+
+//     User.findOne({ email: email }).then((userExist) => {
+//         if (userExist) {
+//             return res.status(422).json({ "error": "Email already exist" })
+//         }
+//         const user = new User({ name, email, phone, work, password });
+//         user.save().then(() => {
+//             res.status(201).json({ "message": "data pushed successfully" }).catch((err) => res.status(500).json({ "err": "Failed to registered" }))
+//         })
+//     }).catch(err => { console.log(err) })
+// return res.status(200);
+// console.log(name, email);
+// console.log(req.body.name, req.body.email);
+// res.json({ "user": req.body });
+// })
+
+// using async await 
+router.post("/register", async (req, res) => {
     const { name, email, phone, work, password } = req.body;
 
     if (!name || !email || !phone || !work || !password) { return res.status(422).json({ "error": "plz fill the form" }) }
 
-    User.findOne({ email: email }).then((userExist) => {
+    try {
+        const userExist = await User.findOne({ email: email });
         if (userExist) {
             return res.status(422).json({ "error": "Email already exist" })
         }
         const user = new User({ name, email, phone, work, password });
-        user.save().then(() => {
-            res.status(201).json({ "message": "data pushed successfully" }).catch((err) => res.status(500).json({ "err": "Failed to registered" }))
-        })
-    }).catch(err => { console.log(err) })
-    // return res.status(200);
-    // console.log(name, email);
-    // console.log(req.body.name, req.body.email);
-    // res.json({ "user": req.body });
+        const userRegister = await user.save();
+        if (userRegister) {
+            res.status(201).json({ "message": "Data pushed successfully" })
+        }
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = router;
